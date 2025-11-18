4ea4ece940475f7d1593ded5611ccea50051214a
ryan-bennett <ryan.bennett.au@gmail.com>
2025-11-19 08:37:22 +1000

    Add coverage to build actions

--- jest.config.ts
+++ jest.config.ts
@@ -4,17 +4,24 @@ const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/src', '<rootDir>/test'],
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+    collectCoverage: false, // Set to true by default if you want coverage on every run
     collectCoverageFrom: [
         'src/**/*.ts',
         '!src/**/*.d.ts',
-        '!src/**/index.ts'
+        '!src/**/*.interface.ts',
+        '!src/**/index.ts', // Typically just exports, can be excluded
     ],
     coverageDirectory: 'coverage',
-    coveragePathIgnorePatterns: ['/node_modules/'],
+    coverageReporters: [
+        'text',           // Console output
+        'text-summary',   // Summary in console
+        'html',          // HTML report in coverage/
+        'lcov',          // For CI/CD tools
+        'json',          // JSON format
+    ],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     verbose: true,
-    testTimeout: 60000, // Increase timeout for container startup
 };
 
 export default config;
